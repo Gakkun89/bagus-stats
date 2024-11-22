d06c75ee7687d3977048012920f61e13e37ff7f5
Mike Warren <mike.warren@aexp.com>
2024-11-22 14:29:18 +0900

    migrate db

--- db/migrate/20241122052431_add_references.rb
+++ db/migrate/20241122052431_add_references.rb
@@ -1,8 +1,8 @@
 class AddReferences < ActiveRecord::Migration[8.0]
   def change
-    add_reference :results, :users, foreign_key: true
-    add_reference :results, :rounds, foreign_key: true
-    add_reference :rounds, :workouts, foreign_key: true
-    add_reference :rounds, :exercises, foreign_key: true
+    add_reference :results, :user, foreign_key: true
+    add_reference :results, :round, foreign_key: true
+    add_reference :rounds, :workout, foreign_key: true
+    add_reference :rounds, :exercise, foreign_key: true
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,66 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2024_11_22_052431) do
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.integer "result"
+    t.text "notes"
+    t.boolean "completed"
+    t.boolean "rx"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "round_id"
+    t.index ["round_id"], name: "index_results_on_round_id"
+    t.index ["user_id"], name: "index_results_on_user_id"
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.integer "number"
+    t.integer "reps"
+    t.integer "order"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "workout_id"
+    t.integer "exercise_id"
+    t.index ["exercise_id"], name: "index_rounds_on_exercise_id"
+    t.index ["workout_id"], name: "index_rounds_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "drop_in"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "type"
+    t.string "name"
+    t.integer "time_cap"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "results", "rounds"
+  add_foreign_key "results", "users"
+  add_foreign_key "rounds", "exercises"
+  add_foreign_key "rounds", "workouts"
+end
