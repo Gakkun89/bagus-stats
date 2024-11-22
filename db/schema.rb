4ac3f81bf16c1e53df3b01fd8308af541ec39e45
Mike Warren <mike.warren@aexp.com>
2024-11-22 14:57:33 +0900

    add basic auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_22_052431) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_22_055705) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -50,6 +50,23 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_052431) do
     t.index ["workout_id"], name: "index_rounds_on_workout_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   create_table "workouts", force: :cascade do |t|
     t.datetime "date"
     t.integer "type"
@@ -63,4 +80,5 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_052431) do
   add_foreign_key "results", "workouts"
   add_foreign_key "rounds", "exercises"
   add_foreign_key "rounds", "workouts"
+  add_foreign_key "sessions", "users"
 end
