b4345afd9af483a8b1d8b3e1bbeb6c89f0d959e8
Mike Warren <mike.warren@aexp.com>
2024-11-26 11:17:25 +0900

    add navbar and basic search

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'bg-blue': '#27496D',
+        'card-blue': '#142850',
+        'text-blue': '#00A8CC',
+        'text-dark-blue': '#0C7B93',
+        'navbar-black': '#0E1423'
+      },
     },
   },
   plugins: [
